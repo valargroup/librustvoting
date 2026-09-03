021d8034642b2274381be9b62e74d43919c742fc
Adam Tucker <adam@osmosis.team>
2026-09-02 23:19:23 -0600

    Move proposal bounds test out of types module

--- zcash_voting/src/types.rs
+++ zcash_voting/src/types.rs
@@ -1174,17 +1174,6 @@ mod tests {
     }
 
     #[test]
-    fn proposal_bounds_match_circuit_authority() {
-        assert!(validate_proposal_id(MIN_PROPOSAL_ID).is_ok());
-        assert!(validate_proposal_id(MAX_PROPOSAL_ID).is_ok());
-        assert!(validate_proposal_id(MAX_PROPOSAL_ID + 1).is_err());
-        assert_eq!(
-            voting_circuits::MAX_PROPOSAL_AUTHORITY,
-            (1u64 << (MAX_PROPOSAL_ID + 1)) - 1
-        );
-    }
-
-    #[test]
     fn vote_decision_validation_rejects_invalid_option_counts() {
         assert!(validate_vote_decision(0, MIN_VOTE_OPTIONS).is_ok());
         assert!(validate_vote_decision(MAX_VOTE_OPTIONS - 1, MAX_VOTE_OPTIONS).is_ok());
